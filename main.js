162e6b62d42b609bb1cff2e04b670172edf01560
Huguini79 <166934655+Huguini79@users.noreply.github.com>
2026-05-16 11:49:51 +0100

    Refactor chart logic and input handling
    Refactor chart rendering and calculations for clarity and efficiency. Introduced miGrafica for chart management and improved input validation.

--- main.js
+++ main.js
@@ -6,10 +6,13 @@ let FiLista = [];
 let HiLista = [];
 let xiPorfi = [];
 
-let sumatorio = 0;
+let miGrafica = null
 
 function MostrarGrafica() {
-  new Chart(document.getElementById("grafica"), {
+    if (miGrafica) {
+        miGrafica.destroy()
+    }
+  miGrafica = new Chart(document.getElementById("grafica"), {
     type: "bar",
     data: {
       labels: data.map((row) => `[${row.start}, ${row.end})`),
@@ -24,6 +27,7 @@ function MostrarGrafica() {
 }
 
 function NEnTotal() {
+    let sumatorio = 0
   data.forEach((element) => {
     sumatorio += element.count;
   });
@@ -32,28 +36,38 @@ function NEnTotal() {
 }
 
 function CalcularFi() {
+    let sumatorio = 0
   data.forEach((element) => {
     sumatorio += element.count;
     FiLista.push(sumatorio);
   });
 }
 
-function AnadirTabla() {
-  let input1 = parseInt(document.getElementById("input1").value);
-  let input2 = parseInt(document.getElementById("input2").value);
-  let fi_entrada = parseInt(document.getElementById("fi_entrada").value);
+function CalcularMedia(N) {
+    let sumatorio = 0
+    xiPorfi.forEach((element) => {
+        sumatorio += element;
+    })
 
-  if (!isNaN(input1)|| !isNaN(input2) || !isNaN(fi_entrada)) {
-    data.push({ start: input1, end: input2, count: fi_entrada });
+    return sumatorio / N;
+}
+
+function AnadirTabla() {
+  let input1 = parseInt(document.getElementById("input1").value)
+  let input2 = parseInt(document.getElementById("input2").value)
+  let fi_entrada = parseInt(document.getElementById("fi_entrada").value)
+
+  if (!isNaN(input1) && !isNaN(input2) && !isNaN(fi_entrada)) {
+    data.push({ start: input1, end: input2, count: fi_entrada })
 
     let contenido = document.getElementById("tabla");
-    contenido.innerHTML = "";
+    contenido.innerHTML = ""
     contenido.innerHTML += `
         <tr>
                             <td>Clases</td>
                             <td>fi</td>
                 </tr>
-        `;
+        `
     data.forEach((element) => {
       contenido.innerHTML += `
                 
@@ -61,7 +75,7 @@ function AnadirTabla() {
                             <td>[${element.start}, ${element.end})</td>
                             <td>${element.count}</td>
                         </tr>
-            `;
+            `
     });
 
   } else {
@@ -70,13 +84,18 @@ function AnadirTabla() {
 }
 
 function Calcular() {
-  let contenido = document.getElementById("tabla");
-  contenido.innerHTML = "";
+  let contenido = document.getElementById("tabla")
+  let resultado = document.getElementById("resultado")
+  xiLista = []
+  hiLista = []
+  FiLista = []
+  HiLista = []
+  xiPorfi = []
+  resultado.innerHTML = ""
+  contenido.innerHTML = ""
   let N = NEnTotal();
-  sumatorio = 0;
-  CalcularFi();
-  sumatorio = 0;
-  let pos = -1;
+  CalcularFi()
+  let pos = -1
   contenido.innerHTML += `
     <tr>
                         <td>Clases</td>
@@ -86,23 +105,21 @@ function Calcular() {
                         <td>Fi</td>
                         <td>Hi</td>
                         <td>xi · fi</td>
-            </tr>
-    `;
+                        </tr>
+                        `;
   data.forEach((element) => {
     pos++;
     let xi =
       (element.start + element.end) /
       2; /* Ej: [0, 4) -> 0 + 4 = 4 -> 4 / 2 = 2 -> xi = 2 */
-    xiLista.push(xi);
-    let hi = element.count / N;
-    hiLista.push(hi);
-    let Fi = FiLista[pos];
-    FiLista.push(Fi);
-    let Hi = Fi / N;
-    HiLista.push(Hi);
-    let xiMultfi = xiLista[pos] * element.count;
-    console.log(xiMultfi);
-    xiPorfi.push(xiMultfi);
+    xiLista.push(xi)
+    let hi = element.count / N
+    hiLista.push(hi)
+    let Fi = FiLista[pos]
+    let Hi = Fi / N
+    HiLista.push(Hi)
+    let xiMultfi = xiLista[pos] * element.count
+    xiPorfi.push(xiMultfi)
     contenido.innerHTML += `
             
                     <tr>
@@ -114,8 +131,13 @@ function Calcular() {
                         <td>${Fi} / ${N}</td>
                         <td>${xiMultfi}</td>
                     </tr>
-        `;
+        `
+  
   });
+
+    let media = CalcularMedia(N).toFixed(2)
+  
+    resultado.innerHTML = `<h2>Media: ${media}</h2>`
 }
 
 function Grafica() {
